03ecd1f909f11abf2c109accfa6ba79d9427d850
Renan Scalanti <renan.scalanti@hotmail.com>
2019-12-09 21:53:25 -0300

    Menu realizado

--- src/App.js
+++ src/App.js
@@ -1,33 +1,88 @@
 import React from 'react';
 import Header from './components/fragment/Header';
+import Footer from './components/fragment/Footer';
+import montarEquipe from './assets/images/montarEquipe-icone.png';
+import viatura from './assets/images/viatura.png';
+import profissional from './assets/images/profissional.png';
+import cliente from './assets/images/cliente.png';
+import unidade from './assets/images/unidade.png';
+import captacao from './assets/images/captacao.png';
+import usuario from './assets/images/usuario.png';
+import paciente from './assets/images/paciente.png';
+import perfilEquipe from './assets/images/perfilEquipe.png';
+
 
 
 function App() {
   return (
     <div>
-    <Header/>
-    <div className="container">
-      <div className="row">
-        <p className="center titulo">Painel administrativo</p>        
-      </div>
-      <div className="row botoes-menu">
-        <div className="col s4">
-          <button>
-
-          </button>
+      <Header />
+      <div className="container">
+        <div className="row m-0">
+          <p className="center titulo">Painel administrativo</p>
         </div>
-        <div className="col s4"></div>
-        <div className="col s4"></div>
-        <div className="col s4"></div>
-        <div className="col s4"></div>
-        <div className="col s4"></div>
+        <div className="row botoes-menu m-0">
+          <div className="col s2 p-0 offset-s3 center">
+            <button>
+              <img className="icone" src={montarEquipe} />
+              <p className="center">Montar equipe</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+              <img className="icone" src={viatura} />
+              <p>Viatura</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+              <img className="icone" src={profissional} />
+              <p>Profissionais</p>
+            </button>
+          </div>
+        </div>
+        <div className="row botoes-menu m-0">
+          <div className="col s2 p-0 offset-s3">
+            <button>
+              <img className="icone" src={cliente} />
+              <p>Clientes</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+              <img className="icone" src={unidade} />
+              <p>Bases e unidades</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+              <img className="icone" src={captacao} />
+              <p>Captação de chamados</p>
+            </button>
+          </div>
+        </div>
+        <div className="row botoes-menu m-0">
+          <div className="col s2 p-0 offset-s3">
+            <button>
+              <img className="icone" src={usuario} />
+              <p>Usuários</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+              <img className="icone" src={paciente} />
+              <p>Pacientes</p>
+            </button>
+          </div>
+          <div className="col s2 p-0">
+            <button>
+            <img className="icone" src={perfilEquipe} />
+              <p>Perfril de equipe</p>
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    <footer>
-      <div className="row">
-          <p className="white-text right-align texto-sigma">SIGMA - Todos os direitos reservados</p>
-      </div>
-    </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/fragment/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <footer>
+                <div className="row">
+                    <p className="white-text right-align texto-sigma">SIGMA - Todos os direitos reservados</p>
+                </div>
+            </footer>
+        );
+    }
+
+}
+export default Footer;
